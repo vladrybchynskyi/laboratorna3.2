f0ddc97b23922c0c7f5c7cbf5f00ddf154dd0a15
vladrybchynskyi <vladyslav.rybchynskyi.ri.2024@lpnu.ua>
2024-09-19 09:33:30 +0300

    Update labor3_2.cpp

--- labor3_2.cpp
+++ labor3_2.cpp
@@ -1,16 +1,23 @@
+// laboratorna 3.2
+// Рибчинський Владислав
+// Лабораторна робота №3.2
+// Розгалуження, задане формулою: функція з параметрами.
+// Варіант 12
+
 # include <iostream>
 # include <cmath>
 using namespace std;
 int main() {
-  double a;
-  double b;
-  double c;
-  double x;
-  double F;
+  double a; // вхідний аргумент
+  double b; // вхідний параметр
+  double c; // вхідний параметр
+  double x; // вхідний параметр
+  double F; // результат обчислення 
   cout << "a = "; cin >> a;
   cout << "b = "; cin >> b;
   cout << "c = "; cin >> c;
   cout << "x ="; cin >> x;
+  // Розгалуження в скороченій формі
   if (x < 0.6 && b + c != 0)
     F = a * x * x + b * b + c;
   if (x > 0.6 && b + c == 0);
@@ -18,6 +25,7 @@ int main() {
   if (!(x <0.6 && b +c != 0) && !(x >0.6 && b + c == 0)) ; 
   F = x / c + x / a;
   cout << "1) F=" << F << endl;
+  // Розгалуження в повній формі
   if (x < 0.6 && b + c != 0)
     F = a * x * x * x + b * b + c;
   else
